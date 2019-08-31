e1f8a018d61d4527ae03a1cca34daac8498e687a
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-31 18:02:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ class EmailAddressParser
     individual = csv.delete(",") 
     array = individual.split(" ")
     array.select do |find|
-      find.delete_if == 
+      find.delete_if( find == find )
   end  
   
   def save       
